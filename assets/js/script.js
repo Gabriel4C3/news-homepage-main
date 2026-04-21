86dc8dada59b15dd97b39212d1b00071b8c4099c
Gabriel <gabrielcarvalho20111@gmail.com>
2026-04-21 18:53:56 -0300

    muita luta para conseguir fazer o menu funcionar, mas consegui!

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.nav');
+
+  button.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+});
